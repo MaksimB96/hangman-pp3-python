b1f4568988fc1327dd6503613a19fef0ce2080cc
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-01-13 15:16:54 +0000

    Add import of modules and lirbraries and tested colorama with print staatement

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import colorama
+import random
+from colorama import Fore
+from hangman_titles import title
+
+colorama.init(autoreset = True)
+
+print(Fore.YELLOW + title)
