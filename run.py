5fb1f1c4791d550025a089b2c878edbeedf8fde5
MaksimB96 <maksims.buraks.1996@gmail.com>
2023-01-15 22:07:56 +0000

    Add google auth, gspread and connected spreadsheet

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 import colorama
+import gspread
 import random
 from colorama import Fore
+from google.oauth2.service_account import credentials
 from hangman_titles import title
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = credentials.from_service_account_file('creds.json') 
+SCOPED_CREDS = CREDS.with_scopes(CREDS)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('passwords-users')
 
 colorama.init(autoreset = True)
 
@@ -12,4 +25,4 @@ def title_intro():
 
     print(Fore.GREEN + title)
     print('Welcome to hangman!')
-    
+
